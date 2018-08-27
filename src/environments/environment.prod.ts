3695df7c3a100df66e05f316de3189c1a00b7ca7
Mason Secky-Koebel <mseckykoebel@gmail.com>
2018-08-27 15:04:52 -0400

    initial commit -> added the page to firebase, added the root app components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBhGvp9W-9ElM7laKT0yYlHCVTdhuL8J2I",
+    authDomain: "oshop-a1bc4.firebaseapp.com",
+    databaseURL: "https://oshop-a1bc4.firebaseio.com",
+    projectId: "oshop-a1bc4",
+    storageBucket: "oshop-a1bc4.appspot.com",
+    messagingSenderId: "1086638838191"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,17 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// All of the settings for the evelopment environment
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBhGvp9W-9ElM7laKT0yYlHCVTdhuL8J2I",
+    authDomain: "oshop-a1bc4.firebaseapp.com",
+    databaseURL: "https://oshop-a1bc4.firebaseio.com",
+    projectId: "oshop-a1bc4",
+    storageBucket: "oshop-a1bc4.appspot.com",
+    messagingSenderId: "1086638838191"
+  }
 };
 
 /*
