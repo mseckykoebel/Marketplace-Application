1cfdb3ef35211761688af5d420a4c07ab302e46b
Mason Secky-Koebel <mseckykoebel@gmail.com>
2018-08-30 15:24:34 -0400

    cleaned up some issues with errors

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-my-orders',
-  templateUrl: './my-orders.component.html',
-  styleUrls: ['./my-orders.component.css']
+  selector: "app-my-orders",
+  templateUrl: "./my-orders.component.html",
+  styleUrls: ["./my-orders.component.css"]
 })
 export class MyOrdersComponent implements OnInit {
 
